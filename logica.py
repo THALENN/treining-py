d9abf06458ccc7d490600717019c318c919a2b98
Thalen <thalen.priscila@hotmail.com>
2024-02-17 12:18:08 -0300

    python

---
+++ logica.py
@@ -0,0 +1,23 @@
+##Tipos de Variaveis
+
+#String ou texto
+nome = 'Thalen'
+sobrenome = 'Ramos'
+
+print(nome, sobrenome)
+
+nome = 'Priscila'
+
+print('O nome agora é priscila')
+
+#integrar ou numero inteiro ===========
+idade = 25
+idade2 = 15
+
+totalidade = idade + idade2
+
+print("a idade total é:", totalidade)
+
+idade2 = 40
+totalidade = idade + idade2
+print("a idade total é:", totalidade)
